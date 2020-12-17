9cceb0bc67141e1f5dfc283c93c8609d81c30b78
Jasmine Rose <jasmine.raquel.rose@gmail.com>
2020-12-17 02:16:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["X", "X", "X", "O", "0", "X", "", "", ""]
+board = ["X", "X", "X", "O", "0", "X", "0", "X", "0"]
 def display_board(board)
   puts " #{board[0]}  | #{board[1]}  | #{board[2]}  "
   puts "-----------"
